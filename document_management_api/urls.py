15fbb980827d4aec48de5e03425dc6b7cfe6cc08
Sydul_amin <73732800+Sydulamin@users.noreply.github.com>
2023-07-20 14:23:05 +0600

    Update urls.py
    changed some text

--- document_management_api/urls.py
+++ document_management_api/urls.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+?from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -8,10 +8,7 @@ schema_view = get_schema_view(
     openapi.Info(
         title="API Documentation",
         default_version="v1",
-        description="API documentation for your project",
-        terms_of_service="https://www.example.com/terms/",
-        contact=openapi.Contact(email="contact@example.com"),
-        license=openapi.License(name="BSD License"),
+        description="API documentation",
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
